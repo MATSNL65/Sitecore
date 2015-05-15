9cc1dcc5eea3c7c3560e16f1176a375b7c2cb120
Kunuk Nykjær <kunukn@gmail.com>
2015-05-16 00:58:08 +0200

    add example

---
+++ startbootstrap-creative example/Sitecore8/Models/ItemHelper.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+
+namespace Sitecore8.Models
+{
+    public static class ItemHelper
+    {
+        public static Item GetDataSourceTargetItemByFieldName(this Item item, string fieldName)
+        {
+            DatasourceField linkDataSource = item.Fields[fieldName];
+            return linkDataSource.TargetItem;            
+        }
+    }
+}
